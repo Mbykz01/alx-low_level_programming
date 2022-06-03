b7e42377be6330c53bc0d35debf06a090cbb82b1
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-03 14:06:25 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* main entry point */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle \n");
+puts(""\n Programming is like building a multilingual puzzle");
 return(0);
 }
