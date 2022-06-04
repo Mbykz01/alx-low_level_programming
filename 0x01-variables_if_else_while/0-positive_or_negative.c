78399b2914c388c0234c3e3d19bc3df0a32ef9af
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-04 13:43:49 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * positive_or_negative 
+ *
+ * Description: Positive anything is better than negative nothing
+ */
+int main(void)
+
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+return (0);
+}
