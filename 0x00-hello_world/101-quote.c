0a0e46b5761d3305429b0dba66c8090d30042013
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-04 07:57:54 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- *Description: Prints "and that piece of art is useful.." without puts
- * program should return: 1
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
