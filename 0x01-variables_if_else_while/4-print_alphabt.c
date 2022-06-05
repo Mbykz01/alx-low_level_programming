f10746ac2901a5851799ad670cdb0ed789cda54e
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-05 07:56:13 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,15 +13,13 @@ int main(void)
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch != 'q')							{										putchar(ch);
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
 }
 else
-{
-
 }
-										}
 putchar('\n');
-
 return (0);
 
 }
