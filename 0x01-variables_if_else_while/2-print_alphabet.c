5d6cee4ecd0f7cd730341a2fe489ebd62f2559aa
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-05 05:53:17 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: program that prints the alphabet in lowercase, followed by a new line
+ * Description: program that prints the alphabet in lowercase
  * Return: Always 0 (Success)
  */
 int main(void)
