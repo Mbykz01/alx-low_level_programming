bac3d1c5c67f106f0233c8b00e8191eb860e83b7
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-05 09:00:14 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * program that prints all single digit numbers of base 10 starting from 0 
+ * Return: 0
+ */
+
+int main(void)
+{
+
+int n = 48;
+while (n <= 57)
+
+{
+putchar(n);
+n += 1;
+}
+
+printf("\n");
+
+return (0);
+
+}
