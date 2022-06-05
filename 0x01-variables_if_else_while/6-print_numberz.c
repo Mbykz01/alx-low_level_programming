e90cdae7b5da80d760f188cf34b0291e7f07119d
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-05 09:23:37 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Entry
- * program that prints all single digit numbers of base 10 starting from 0 
+ * program that prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
 
 int main(void)
 {
-
 int k;
 for (k = 48; k <= 57; k++)
 {
