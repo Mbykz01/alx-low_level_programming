0fd843059ae8985d6c10949174d68642458c2c3d
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-09 10:25:49 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 52 fibonacci numbers
- * Return: Nothing!
+ * main - Prints the sum of all multiples of 3 or 5 up to 102
+ * Return: Always (Success)
  */
 
 int main(void)
 
 {
-
-int i = 0;
-long j = 1, k = 2;
-while (i < 50)
+int i, z = 0;
+while (i < 1024)
 {
-if (i == 0)
-printf("%ld", j);
-else if (i == 1)
-printf(", %ld", k);
-else
+if ((i % 3 == 0) || (i % 5 == 0))
 {
-k += j;
-j = k - j;
-printf(", %ld", k);
+z += i;
 }
-++i;
+i++;
 }
-printf("\n");
+printf("%d\n", z);
 
 return (0);
 
