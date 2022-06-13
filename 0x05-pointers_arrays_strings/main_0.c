bec0b290099ff979517851fc9a91d9baec348ee7
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-06-13 13:46:19 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void _puts(char *);
+
+/**
+ * main - check the code for main School students.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char *str;
+str = "main!";
+_puts(str);
+return (0);
+
+}
