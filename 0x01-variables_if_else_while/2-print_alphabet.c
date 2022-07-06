6c8f5211382134a02fc972b837d69a9df1ac17dc
Abdulkarim Ibrahim Aminu <aminuabdulkarimibrahimu@gmail.com>
2022-07-06 04:02:51 -0700

    task 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
