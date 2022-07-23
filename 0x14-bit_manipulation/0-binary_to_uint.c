b2e0631f95d855b02375a10ea22622533dc07ced
Abdulkarim Ibrahim Aminu <104824540+Mbykz01@users.noreply.github.com>
2022-07-23 22:37:19 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	int a;
+	unsigned int dec_val = 0;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (a = 0; b[a]; a++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		dec_val = 2 * dec_val + (b[a] - '0');
 	}
 
-	return (num);
+	return (dec_val);
 }
