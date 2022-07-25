4972d4962002cb82109bb37b01333db73d369ca4
Abdulkarim Ibrahim Aminu <104824540+Mbykz01@users.noreply.github.com>
2022-07-25 15:16:20 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,65 +1,33 @@
 #include "main.h"
 
-
-
 /**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
  *
- *  * append_text_to_file - appends text at the end of a file
- *
- *   * @filename: file to append the text to
- *
- *    * @text_content: content to append into the file
- *
- *     *
- *
- *      * Return: 1 on success and -1 on failure
- *
- *       */
-
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
-
 {
+	int k, r, e = 0;
 
-		int k, r, e = 0;
+	if (!filename)
+		return (-1);
 
+	k = open(filename, O_WRONLY | O_APPEND);
+	if (k < 0)
+		return (-1);
 
+	if (text_content)
+	{
+		while (text_content[e])
+			e++;
+		r = write(k, text_content, e);
+		if (r != e)
+			return (-1);
+	}
 
-			if (!filename)
+	close(k);
 
-						return (-1);
-
-
-
-				k = open(filename, O_WRONLY | O_APPEND);
-
-					if (k < 0)
-
-								return (-1);
-
-
-
-						if (text_content)
-
-								{
-
-											while (text_content[e])
-
-															e++;
-
-													r = write(k, text_content, e);
-
-															if (r != e)
-
-																			return (-1);
-
-																}
-
-
-
-							close(k);
-
-
-
-								return (1);
-
+	return (1);
 }
